08ceb7bde7edc5a0854b94386892812bab7d47e9
Heang Chihav <heangchihav@gmail.com>
2026-02-14 11:37:52 +0700

    updated folder structure

--- infra/docker/initdb/create-databases.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE user_service_db;
-CREATE DATABASE call_service_db;
-CREATE DATABASE marketing_service_db;
-CREATE DATABASE delivery_service_db;
-CREATE DATABASE branchreport_service_db;
